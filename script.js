6aa2397fbc6079226c39a1a81485e00bf6f52288
roddejos <roddejos@gmail.com>
2023-10-01 14:48:53 +0800

    update all

--- script.js
+++ script.js
@@ -37,3 +37,69 @@ function reveal(){
     }
   }
 }
+
+window.addEventListener('scroll', revealUp);
+function revealUp(){
+  var reveals = document.querySelectorAll('.reveal-up');
+  
+  for (var i = 0; i < reveals.length; i++){
+    let windowHeight = window.innerHeight;
+    let revealTop = reveals[i].getBoundingClientRect().top;
+    let revealPoint = 15;
+
+    if(revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add('active');
+    }
+
+    else {
+      reveals[i].classList.remove('active');
+    }
+  }
+}
+
+
+// HOVER EFFECT
+// const elements = document.querySelectorAll('.hover-gradient-text');
+// const newClass = 'gradient-text';
+
+// elements.forEach(element => {
+//   element.addEventListener('mouseover', () => {
+//     element.classList.add(newClass);
+//   });
+
+//   element.addEventListener('mouseout', () => {
+//     element.classList.remove(newClass);
+//   });
+// })
+
+const elements = document.querySelectorAll('.cursor2');
+const links = document.querySelectorAll('.link-hover');
+const hoverClass = 'cursor-hover';
+const clickClass = 'cursor-click';
+
+links.forEach(link => {
+  link.addEventListener('mouseover', () => {
+    elements.forEach(element => {
+      element.classList.add(hoverClass);
+    });
+  });
+
+  link.addEventListener('mouseout', () => {
+    elements.forEach(element => {
+      element.classList.remove(hoverClass);
+    });
+  });
+
+  link.addEventListener('mousedown', () => {
+    elements.forEach(element => {
+      element.classList.add(clickClass)
+    })
+  })
+
+  link.addEventListener('mouseup', () => {
+    elements.forEach(element => {
+      element.classList.remove(clickClass)
+    })
+  })
+
+});
